7bb312d7bf6cfee7e144f5bce021fbe2a35e8007
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-04 20:58:12 +0300

    Uygulamada ürün adı, fiyatı ve kategori ekleme kısımları eklendi; ayrıca chechbox mantığı ile tamamlama UI eklendi

--- js/main4.js
+++ js/main4.js
@@ -14,13 +14,22 @@ function generateID(){
 }
 
 function addProduct(){
-    const input = document.getElementById("textInput");
-    const value = input.value;
+    const nameInput = document.getElementById("nameInput");
+    const priceInput = document.getElementById("priceInput");
+    const categoryInput = document.getElementById("category")
 
-    const urun = Number(value);
+    const name = nameInput.value;
+    const price = Number(priceInput.value);
+    const category = categoryInput.value;
+
+    if (name === "" || price === 0);
+
     const newItem = {
         id: generateID(),
-        urun: urun,
+        name: name,
+        price: price,
+        category: category,
+        completed: false,
     };
 
     product.push(newItem);
@@ -28,7 +37,9 @@ function addProduct(){
 
     renderProduct();
 
-    input.value = "";
+    nameInput.value = "";
+    priceInput.value = "";
+
 }
 
 function renderProduct(){
@@ -37,7 +48,21 @@ function renderProduct(){
 
     product.forEach(function(item){
         const li = document.createElement("li");
-        li.textContent = item.urun;
+        li.textContent = item.name + " " + item.price + " ₺ (" + item.category + ")";
+
+        const checkbox = document.createElement("checkbox");
+        checkbox.type = "checkbox";
+        checkbox.checked = item.completed;
+
+        checkbox.onchange = function(){
+            item.completed = !item.completed;
+            localStorage.setItem("product", JSON.stringify(product));
+            renderProduct();
+        }
+
+        if(item.completed){
+            li.style.textDecoration = "line-through";
+        }
 
         const btn = document.createElement("button");
         btn.textContent = "SİL";
@@ -48,11 +73,11 @@ function renderProduct(){
             renderProduct();
         }
 
+        li.appendChild(checkbox);
         li.appendChild(btn);
         list.appendChild(li);
     })
 
-    const urunler = product.map(item => item.urun);
-    const toplam = urunler.reduce((acc, val) => acc + val, 0);
+    const toplam = product.reduce((acc, item) => acc + item.price, 0);
     document.getElementById("balance").textContent = toplam + " ₺";
 }
